2cae1aa655641f1ad558f2538f499905446981fa
Emre Toraman <emretoraman@gmail.com>
2022-07-21 23:23:35 +0300

    UserService implementation

--- CloudCustomers.API/Services/UsersService.cs
+++ CloudCustomers.API/Services/UsersService.cs
@@ -1,4 +1,7 @@
-﻿using CloudCustomers.API.Models;
+﻿using CloudCustomers.API.Config;
+using CloudCustomers.API.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
 
 namespace CloudCustomers.API.Services
 {
@@ -10,16 +13,27 @@ namespace CloudCustomers.API.Services
     public class UsersService : IUsersService
     {
         private readonly HttpClient _httpClient;
+        private readonly UsersApiOptions _apiConfig;
 
-        public UsersService(HttpClient httpClient)
+        public UsersService(
+            HttpClient httpClient,
+            IOptions<UsersApiOptions> apiConfig
+        )
         {
             _httpClient = httpClient;
+            _apiConfig = apiConfig.Value;
         }
 
         public async Task<List<User>> GetAllUsers()
         {
-            var usersResponse = await _httpClient.GetAsync("https://example.com");
-            return new List<User>() { };
+            var usersResponse = await _httpClient.GetAsync(_apiConfig.Endpoint);
+            if (usersResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                return new List<User>();
+            }
+            var responseContent = usersResponse.Content;
+            var allUsers = await responseContent.ReadFromJsonAsync<List<User>>();
+            return allUsers!;
         }
     }
 }
